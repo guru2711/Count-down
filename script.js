5f2d968d1431d72c859dc1da524eb75a9ee46368
guru2711 <saiguruprakash@gmail.com>
2021-08-16 18:58:30 +0530

    count down

---
+++ script.js
@@ -0,0 +1,46 @@
+setTimeout(function () {
+   document.body.innerHTML = `<h1>10</h1>`
+   document.body.style.backgroundColor = "red"
+     setTimeout(function () {
+         document.body.innerHTML = `<h1>9</h1>`
+         document.body.style.backgroundColor = "yellow"
+         setTimeout(function () {
+           document.body.innerHTML = `<h1>8</h1>`
+           document.body.style.backgroundColor = "snow"
+   setTimeout(function () {
+           document.body.innerHTML = `<h1>7</h1>`
+           document.body.style.backgroundColor = "pink"
+ setTimeout(function () {
+           document.body.innerHTML = `<h1>6</h1>`
+           document.body.style.backgroundColor = "yellow"
+ setTimeout(function () {
+           document.body.innerHTML = `<h1>5</h1>`
+           document.body.style.backgroundColor = "orange"
+ setTimeout(function () {
+           document.body.innerHTML = `<h1>4</h1>`
+           document.body.style.backgroundColor = "snow"
+ setTimeout(function () {
+           document.body.innerHTML = `<h1>3</h1>`
+           document.body.style.backgroundColor = "pink"
+ setTimeout(function () {
+           document.body.innerHTML = `<h1>2</h1>`
+           document.body.style.backgroundColor = "red"
+ setTimeout(function () {
+           document.body.innerHTML = `<h1>1</h1>`
+           document.body.style.backgroundColor = "orange"
+ setTimeout(function () {
+           document.body.innerHTML = `<h1>Happy Independence Day </h1>`
+           document.body.style.backgroundColor = "yellow"
+
+         }, 1000)
+         }, 1000)
+         }, 1000)
+         }, 1000)
+         }, 1000)
+         }, 1000)
+         }, 1000)
+         }, 1000)
+         }, 1000)
+     }, 1000)
+
+ }, 1000)
